78b4082f18f4dd6537c0b35434da9378a6543b35
Eisuke Kuwahata <kuwahata_eisuke@aratana.jp>
2018-09-28 17:00:06 +0900

    Hello, world! with Browser module.

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,39 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (Html, text)
+import Browser
 
-main = text "Hello, world!"
+main: Program () Model Msg 
+main = Browser.sandbox 
+  { init = init
+  , update = update
+  , view = view
+  }
+
+type alias Model =
+  { timeline: Timeline }
+
+type alias Tweet = 
+  { message: String
+  , username: String
+  }
+
+type alias Timeline = List Tweet
+
+init : Model
+init = 
+  { timeline = [] }
+
+type Msg =
+  NoOp
+  
+update : Msg -> Model -> Model
+update msg model =
+  case msg of 
+    NoOp ->
+      model
+      
+
+view : Model -> Html Msg
+view model =
+    text "Hello, world!"
